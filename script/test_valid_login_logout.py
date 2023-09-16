e0e16e99c173fa4076edbe5ca9ef4ee4d870ed03
AksharaBhanu <javagalbhanu@gmail.com>
2023-09-16 07:39:08 +0530

    Automated test case 1-> valid login

--- script/test_valid_login_logout.py
+++ script/test_valid_login_logout.py
@@ -8,7 +8,7 @@ from generic.excel import Excel
 
 class Test_ValidLogin(Base_SetUp):
 
-    @pytest.mark.run(order=1)
+    @pytest.mark.run(order=3)
     def test_valid_login(self):
         un = Excel.get_cell_data(self.XL_PATH, "validlogin", 2, 1)
         pw = Excel.get_cell_data(self.XL_PATH, "validlogin", 2, 2)
@@ -19,7 +19,9 @@ class Test_ValidLogin(Base_SetUp):
         loginpage.set_password(pw)
         # 3. Click on login Button
         loginpage.click_login_button()
-        # 4. Verify that Home page is Displayed
+        # 4. click on logout link
         homepage = EnterTimeTrackPage(self.driver)
-        status = homepage.verify_home_page_displayed(self.wait)
+        homepage.click_logout()
+        # 5. Verify that  login is Displayed
+        status=loginpage.verify_login_page_displayed(self.wait)
         assert status
